715ac1ed7050db3c7e2fd7963c5ab600b80fc6ea
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-13 19:50:36 +0900

    Glide를 이용한 이미지 로딩

--- app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_01_libraries/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.tjoeun.a20191113_01_libraries
 
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -12,6 +14,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        var imgUrl = "https://img1.yna.co.kr/photo/yna/YH/2017/12/02/PYH2017120207500001300_P2.jpg"
+        Glide.with(mContext).load(imgUrl).into(mainCirImg)
     }
 
     override fun setValues() {
